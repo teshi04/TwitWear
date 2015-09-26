964c09f072c0ed788c9dc9b81f7afdf66895a4b5
teshi04 <teshi04@gmail.com>
2015-09-26 14:58:02 +0900

    インジケータが表示されるように修正

--- wear/src/main/java/jp/tsur/twitwear/ui/TimelineActivity.java
+++ wear/src/main/java/jp/tsur/twitwear/ui/TimelineActivity.java
@@ -1,11 +1,9 @@
 package jp.tsur.twitwear.ui;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.wearable.view.DotsPageIndicator;
 import android.support.wearable.view.GridViewPager;
-import android.view.View;
-import android.view.WindowInsets;
 
 import java.util.ArrayList;
 
@@ -19,26 +17,17 @@ public class TimelineActivity extends Activity {
     @InjectView(R.id.pager)
     GridViewPager mPager;
 
+    @InjectView(R.id.indicator)
+    DotsPageIndicator indicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         ButterKnife.inject(this);
 
-        final Resources res = getResources();
-        mPager.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
-            @Override
-            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
-                final boolean round = insets.isRound();
-                int rowMargin = res.getDimensionPixelOffset(R.dimen.page_row_margin);
-                int colMargin = res.getDimensionPixelOffset(round ?
-                        R.dimen.page_column_margin_round : R.dimen.page_column_margin);
-                mPager.setPageMargins(rowMargin, colMargin);
-                return insets;
-            }
-        });
-
         ArrayList<Page> pages = (ArrayList<Page>) getIntent().getSerializableExtra("pages");
         mPager.setAdapter(new SampleGridPagerAdapter(this, getFragmentManager(), pages));
+        indicator.setPager(mPager);
     }
 }
